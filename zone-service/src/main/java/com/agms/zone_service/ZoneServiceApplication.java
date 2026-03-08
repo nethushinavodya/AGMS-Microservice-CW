20e88cdd63f84aa9a317d3fca595ee017f4419bb
nethushinavodya <nethushiperera@gmail.com>
2026-03-08 17:56:25 +0530

    feat: enable Feign clients in ZoneServiceApplication

--- zone-service/src/main/java/com/agms/zone_service/ZoneServiceApplication.java
+++ zone-service/src/main/java/com/agms/zone_service/ZoneServiceApplication.java
@@ -2,12 +2,13 @@ package com.agms.zone_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ZoneServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZoneServiceApplication.class, args);
 	}
-
 }
