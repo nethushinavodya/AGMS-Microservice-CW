520917404003bf65d4c05b9a51875ee9b36e0615
nethushinavodya <nethushiperera03@gmail.com>
2026-04-09 00:22:21 +0530

    feat: update application configuration for datasource and enable Feign clients

--- Sensor-Service/src/main/java/com/agms/sensor_service/SensorServiceApplication.java
+++ Sensor-Service/src/main/java/com/agms/sensor_service/SensorServiceApplication.java
@@ -2,12 +2,14 @@ package com.agms.sensor_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableDiscoveryClient
+@EnableFeignClients
 public class SensorServiceApplication {
 
 	public static void main(String[] args) {
